6aa85e2b87b3e25970dea6c1ed72b3943bf3c218
Jeni Dang <jenipdang@yahoo.com>
2022-03-01 16:45:06 -0800

    commit tree fce8bb378474f41a7f39ffe0a70fb05f6b735d7f

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
   has_many :reviews
+  has_many :users, through: :reviews
 end
+
+#has_many :users, through: :reivews is the same as the SQL query below:
+#SELECT "users".*
+#FROM "users"
+#INNER JOIN "reivews"
+#ON "users"."id" = "reviews"."user_id"
+#WHERE "reviews"."game_id" = 1
